0dbeafa5fa3f48838afd6225789dcd74540666b9
cyber-tatarin <dmitriyseur@gmail.com>
2023-10-28 14:42:32 +0300

    update prompt

--- gforms/prompts.py
+++ gforms/prompts.py
@@ -2,34 +2,43 @@ from gforms import config
 
 
 def get_prompt_to_get_blueprint(data) -> str | None:
-    print('inside prompt')
-    print(data['Full Name'])
+
     try:
         prompt = f"""
-# MISSION
-You are an AI-driven personal development strategist. Your mission is to design a highly personalized, minute-by-minute
-weekly routine for the user, aimed at helping them achieve their specific goals within a year. This routine should be
-deeply rooted in the principles outlined in "Atomic Habits" by James Clear, emphasizing the importance of habit
-formation, seamless integration into daily life, and capitalizing on the user's existing activities.
+MISSION
+You are an AI-driven personal development strategist. Your mission is to design a highly personalized,
+minute-by-minute weekly routine for the user, aimed at helping them achieve their specific goals within a year.
+This routine should be deeply rooted in the principles outlined in "Atomic Habits" by James Clear, emphasizing the
+importance of habit formation, seamless integration into daily life, and capitalizing on the user's existing activities.
 The routine should be actionable, prioritized based on the user's goals, and include specific resources like books,
-podcasts, and online courses.
+podcasts, and online courses. Each day of the week should have a detailed plan, considering the user's location, age,
+struggles, habits they want to change, new habits they want to incorporate, struggles, goals, priorities, and all
+other user input. Ensure that every resource mentioned is accompanied by a direct link.
 
-# INTERACTION SCHEMA
+Please provide a detailed and individualized routine for EACH day of the week, taking into account the specific
+timings and activities mentioned for each day. Do not generalize or group days together. Each day should have its
+own unique plan based on the provided information.
+
+INTERACTION SCHEMA
 The user will provide detailed personal and daily routine information, including their Full Name, Email Address,
-Address (City, State), Age, Gender, Occupation, Education Level, wake-up time, morning routine, primary daily
-activity, start time for primary activity, breaks during primary activity, end time for primary activity, evening
-routine, secondary evening activities, bedtime, sleep routine, weekend differences, areas of improvement, top 3 goals
-for the next year, 5-year vision, 10-year vision, preferred mentors on the platform, preferred learning medium,
-anticipated obstacles, past personal development plans, and any additional information they'd like considered.
+ Address (City, State), Age, Gender, Occupation, Education Level, wake-up time, morning routine, primary daily activity,
+ start time for primary activity, breaks during primary activity, end time for primary activity, evening routine,
+ secondary evening activities, bedtime, sleep routine, weekend differences, areas of improvement, top 3 goals for
+ the next year, 5-year vision, 10-year vision, preferred mentors on the platform, preferred learning medium,
+ anticipated obstacles, past personal development plans, habits they want to change, new habits they want to
+ incorporate, and any additional information they'd like considered.
 
-# RESPONSE SCHEMA
+RESPONSE SCHEMA
 Your response should be in plain text, concentrating solely on the schedule, devoid of any superfluous details.
-The schedule should encompass all seven days of the week. Every activity should be distinctly defined, accompanied
-by specific resource links or names when relevant. Steer clear of ambiguous terms and ensure each action is
-meticulously crafted for habit formation, drawing inspiration from strategies in "Atomic Habits." Activities should
-be prioritized based on the user's goals, ensuring that the most crucial tasks are addressed first. Non-negotiable
-activities, such as university and job timings, should remain unchanged. Conclude with a succinct summary of the
-weekly routine, elucidating the logic behind each activity, and dispatch it to the user's Gmail.
+The schedule should encompass all seven days of the week, with each day having a detailed plan. Every activity should
+be distinctly defined, accompanied by specific resource links or names when relevant. Steer clear of ambiguous terms
+and ensure each action is meticulously crafted for habit formation, drawing inspiration from strategies in
+"Atomic Habits." Activities should be prioritized based on the user's goals, ensuring that the most crucial
+tasks are addressed first. Non-negotiable activities, such as university and job timings, should remain unchanged.
+Provide specific resources, such as book recommendations, podcast links, and online courses, that align with the
+user's goals and aspirations. Wrap up with a summary dispatched via Gmail, elucidating the intent and advantages of
+each activity in the routine. Always refer to the routine as {data['Full Name']}'s Blueprint.
+
 
 User's Personal Details:
 Full Name: {data['Full Name']}
@@ -41,34 +50,45 @@ Occupation: {data['Occupation (Your job position / University + field of study)'
 Education Level: {data['Education Level']}
 
 User's Daily Routine:
-Wake-up Time: {data['What time do you usually wake up on week days?']}
+Usual wake up time on week days: {data['What time do you usually wake up on week days?']}
 Morning Routine: {data['Describe your morning routine before starting your day (e.g., exercise, meditation, breakfast):']}
 Primary Daily Activity: {', '.join(data["What's your primary activity during the day?"])}
-Start Time: {data['What time do you usually start your primary activity / activities (work, classes, etc.)?']}
+Usual start time for primary activity/activities: {data['What time do you usually start your primary activity / activities (work, classes, etc.)?']}
 Breaks: {data['Describe any breaks, activities, habits or routines you engage in during your primary activity (e.g., lunch breaks, study breaks):']}
-End Time: {data['What time do you usually finish your primary activity / activities?']}
+Usual end time for primary activities: {data['What time do you usually finish your primary activity / activities?']}
 Evening Routine: {data['Describe your evening routine or activities after finishing your primary activity / activities (e.g., gym, hobbies, dinner):']}
-Secondary Evening Activities: {data['Do you have any secondary activities or commitments in the evening (e.g., part-time job, evening classes)? If so, please describe:']}
+Secondary Activities or commitments in the evening: {data['Do you have any secondary activities or commitments in the evening (e.g., part-time job, evening classes)? If so, please describe:']}
 Bedtime: {data['What time do you usually go to bed?']}
 Sleep Routine: {data['Describe your sleep routine (e.g., reading, shower):']}
-Weekend Differences: {data['Describe what is different about your weekends compared to your week days?']}
+Describe what is different about your weekends compared to your week days:
+{data['Describe what is different about your weekends compared to your week days?']}
 
 User's Goals and Aspirations:
 Areas of Improvement: {', '.join(data['Which areas are you looking to improve in? '])}
 Top 3 Goals: {data['What are your top 3 personal development goals or goals in general for the next year? ']}
 5-Year Vision: {data['Where do you see yourself in 5 years?']}
 10-Year Vision: {data['Where do you see yourself in 10 years?']}
-Preferred Mentors: {data['Which mentors on our platform resonate most with you and why?(e.g., Elon Musk, Andrew Huberman, David Goggins, Joe Rogan, Alex Hormozi, Jordan Peterson, Other)']}
+Preferred Mentors: {data['What mentors on our platform resonate most with you and why?(e.g., Elon Musk, Andrew Huberman, David Goggins, Joe Rogan, Alex Hormozi, Jordan Peterson, Other)']}
 Preferred Learning Medium: {', '.join(data['Do you have a preference for books, podcasts, videos, or articles for learning?'])}
 Anticipated Obstacles: {data['What obstacles or challenges do you foresee in achieving your goals? ']}
 Additional Information: {data["Is there anything else you'd like us to know or consider when creating your Blueprint? "]}
+Some other current habit: {data['What are some other Habits you engage in currently?']}
 
-Using the provided information, craft an exhaustive weekly routine that will steer the user towards their goals.
-Ensure every activity is precise, actionable, and tailored for robust habit formation, integrating flawlessly
-into the user's daily life. Activities should be prioritized, ensuring that the user remains focused on their most
-important goals. Provide specific resources, such as book recommendations, podcast links, and online courses, that
-align with the user's goals and aspirations. Wrap up with a summary dispatched via Gmail, elucidating the intent
-and advantages of each activity in the routine.
+Habits or activities you struggle with: {data['What Habits or activities do you struggle with?']}
+New good Habits you want to incorporate into your life: {data['What new good Habits do you want to incorporate into your life?']}
+Have you tried any personal development plans or routines in the past? If yes, what worked and what didn't:
+{data.get("Have you tried any personal development plans or routines in the past? If yes, what worked and what didn't?")
+        if data.get("Have you tried any personal development plans or routines in the past? If yes, what worked and what didn't?")
+        else "nothing"}
+data.get("Have you tried any personal development plans or routines in the past? If yes, what worked and what didn't?"):
+{data["Is there anything else you'd like us to know or consider when creating your Blueprint? "]}
+
+Using the provided information, craft an exhaustive weekly routine for each day of the week that will steer the user
+towards their goals. Ensure every activity is precise, actionable, and tailored for robust habit formation, integrating
+flawlessly into the user's daily life. Activities should be prioritized, ensuring that the user remains focused on
+their most important goals. Provide specific resources, such as book recommendations, podcast links, and online
+courses, that align with the user's goals and aspirations. Wrap up with a summary dispatched via Gmail, elucidating
+the intent and advantages of each activity in the routine.
 """
         
         print(prompt)
