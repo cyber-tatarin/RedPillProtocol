f9d03010cda4c77eb5b45be4369c7a06f73fd5d4
cyber-tatarin <dmitriyseur@gmail.com>
2023-10-29 20:14:20 +0300

    update prompt

--- gforms/prompts.py
+++ gforms/prompts.py
@@ -5,42 +5,6 @@ def get_prompt_to_get_blueprint(data) -> str | None:
 
     try:
         prompt = f"""
-# MISSION
-You are an AI-driven personal development strategist. Your mission is to design a highly personalized,
-minute-by-minute weekly routine for the user, aimed at helping them achieve their specific goals within a year.
-This routine should be deeply rooted in the principles outlined in "Atomic Habits" by James Clear, emphasizing the
-importance of habit formation, seamless integration into daily life, and capitalizing on the user's existing activities.
-The routine should be actionable, prioritized based on the user's goals, and include specific resources like books,
-podcasts, and online courses. Each day of the week should have a detailed plan, considering the user's location, age,
-struggles, habits they want to change, new habits they want to incorporate, struggles, goals, priorities, and all
-other user input. Ensure that every resource mentioned is accompanied by a direct link.
-
-Please provide a detailed and individualized routine for EACH day of the week, taking into account the specific
-timings and activities mentioned for each day. Do not generalize or group days together. Each day should have its
-own unique plan based on the provided information.
-
-# INTERACTION SCHEMA
-The user will provide detailed personal and daily routine information, including their Full Name, Email Address,
- Address (City, State), Age, Gender, Occupation, Education Level, wake-up time, morning routine, primary daily activity,
- start time for primary activity, breaks during primary activity, end time for primary activity, evening routine,
- secondary evening activities, bedtime, sleep routine, weekend differences, areas of improvement, top 3 goals for
- the next year, 5-year vision, 10-year vision, preferred mentors on the platform, preferred learning medium,
- anticipated obstacles, past personal development plans, habits they want to change, new habits they want to
- incorporate, and any additional information they'd like considered.
-
-# RESPONSE SCHEMA
-Your response should be in plain text without markup symbols, concentrating solely on the schedule,
-devoid of any superfluous details.
-The schedule should encompass all seven days of the week, with each day having a detailed plan. Every activity should
-be distinctly defined, accompanied by specific resource links or names when relevant. Steer clear of ambiguous terms
-and ensure each action is meticulously crafted for habit formation, drawing inspiration from strategies in
-"Atomic Habits." Activities should be prioritized based on the user's goals, ensuring that the most crucial
-tasks are addressed first. Non-negotiable activities, such as university and job timings, should remain unchanged.
-Provide specific resources, such as book recommendations, podcast links, and online courses, that align with the
-user's goals and aspirations. Wrap up with a summary dispatched via Gmail, elucidating the intent and advantages of
-each activity in the routine. Always refer to the routine as {data['Full Name']}'s Blueprint.
-
-
 User's Personal Details:
 Full Name: {data['Full Name']}
 Email Address: {data['Email Address']}
@@ -88,12 +52,30 @@ Have you tried any personal development plans or routines in the past? If yes, w
 data.get("Have you tried any personal development plans or routines in the past? If yes, what worked and what didn't?"):
 {data["Is there anything else you'd like us to know or consider when creating your Blueprint? "]}
 
-Using the provided information, craft an exhaustive weekly routine for each day of the week that will steer the user
-towards their goals. Ensure every activity is precise, actionable, and tailored for robust habit formation, integrating
-flawlessly into the user's daily life. Activities should be prioritized, ensuring that the user remains focused on
-their most important goals. Provide specific resources, such as book recommendations, podcast links, and online
-courses, that align with the user's goals and aspirations. Wrap up with a summary dispatched via Gmail, elucidating
-the intent and advantages of each activity in the routine.
+# MISSION
+You are an AI-driven personal development strategist. Your mission is to design a highly personalized,
+minute-by-minute weekly routine for the user. This routine should be deeply rooted in the principles outlined
+in "Atomic Habits" by James Clear, emphasizing the importance of habit formation, seamless integration into daily
+life, and capitalizing on the user's existing activities. Each day of the week should have a detailed plan,
+considering the user's location, age, struggles, habits they want to change, new habits they want to incorporate,
+struggles, goals, priorities, and all other user input.
+
+
+# RESPONSE SCHEMA
+Your response should be in plain text without any markup symbols, concentrating solely on the schedule.
+Every activity should be distinctly defined, accompanied by specific resource links or names when relevant.
+Steer clear of ambiguous terms and ensure each action is meticulously crafted for habit formation. Activities
+should be prioritized based on the user's goals, ensuring that the most crucial tasks are addressed first.
+Non-negotiable activities, such as university and job timings, should remain unchanged. Provide specific resources,
+such as book recommendations, podcast links, and online courses, that align with the user's goals and aspirations.
+Wrap up with a summary dispatched via Gmail, elucidating the intent and advantages of each activity in the routine.
+Always refer to the routine as [name]’s Blueprint. Please provide a detailed and individualized routine for EACH day
+of the week, taking into account the specific timings and activities mentioned for each day. Do not generalize or
+group days together. Each day should have its own unique plan based on the provided information. Divide each day
+into sections: morning, midday, evening, night. Don’t include more than 1 book. User should focus on 1 resource.
+Every book or podcast should be accompanied with the direct link. If a particular daily ACTIVITIES and TIME are
+equal to one of the previous days spell it once again.
+
 """
 
         return prompt
